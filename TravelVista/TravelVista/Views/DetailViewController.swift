541c26e9d32caa3a8ae9eb9a8c66b5f5fbdd219e
Amandine Cousin <cousin.amandine.pro@gmail.com>
2023-12-18 16:04:09 +0100

    Ajout de la navigation vers la nouvelle vue detail

---
+++ TravelVista/TravelVista/Views/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  TravelVista
+//
+//  Created by Amandine Cousin on 18/12/2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
