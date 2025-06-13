7867717b4d96ce692a80f7d10ce5767a3442640f
pascaljesenberger <pascal@jesenberger.fr>
2025-06-14 00:34:30 +0200

    Ajout de TitleViewSwiftUI sans modification du template SwiftUI

---
+++ TravelVista/TravelVista/Views/SwiftUI/TitleViewSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  TitleViewSwiftUI.swift
+//  TravelVista
+//
+//  Created by Pascal Jesenberger on 14/06/2025.
+//
+
+import SwiftUI
+
+struct TitleViewSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleViewSwiftUI()
+}
